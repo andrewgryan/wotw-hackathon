f1d3bd82747aefe70e2c2fda3fc7d57a44413fcf
andrewgryan <andrew.ryan@metoffice.gov.uk>
2019-08-20 14:14:48 +0100

    regular commit

--- simple/wotw.js
+++ simple/wotw.js
@@ -13,21 +13,25 @@ wotw = (function() {
     }
 
     ns.main = function() {
-        let select = document.createElement("select")
+        let collectionID = null;
+        let instanceID = null;
+        let collectionSelect = document.createElement("select")
         let instanceSelect = document.createElement("select")
+        let navDiv = document.getElementById("nav")
+        let responseDiv = document.getElementById("response")
+        navDiv.appendChild(collectionSelect)
+        navDiv.appendChild(instanceSelect)
         url = "http://labs.metoffice.gov.uk/wotw/collections?outputFormat=application%2Fjson"
         getRequest(url, function(responseText) {
             JSON.parse(responseText).collections.forEach((collection) => {
                 let option = document.createElement("option")
                 option.value = collection.id
                 option.innerHTML = collection.id
-                select.appendChild(option)
+                collectionSelect.appendChild(option)
             })
         })
-        document.body.appendChild(select)
-        document.body.appendChild(instanceSelect)
-        select.onchange = function(event) {
-            let collectionID = event.target.value
+        collectionSelect.onchange = function(event) {
+            collectionID = event.target.value
             let url = `http://labs.metoffice.gov.uk/wotw/collections/${collectionID}?outputFormat=application%2Fjson`
             getRequest(url, function(responseText) {
                 instanceSelect.innerHTML = ""
@@ -39,44 +43,14 @@ wotw = (function() {
                 })
             })
         }
-
-
-        let queryDiv = document.getElementById("query")
-        let div = document.getElementById("response")
-        url = "http://labs.metoffice.gov.uk/wotw/collections?outputFormat=application%2Fjson"
-
-        let ul = document.createElement('ul')
-        let visit = function(url) {
-            let li = document.createElement('li')
-            li.innerHTML = url
-            ul.appendChild(li)
-        }
-        queryDiv.appendChild(ul)
-
-        visit(url)
-        getRequest(url, function(responseText) {
-            let jsonResponse = JSON.parse(responseText);
-            let ukv = jsonResponse.collections.filter(function(collection) {
-                return collection.id == "ukv_deterministic_surface_239"
-            });
-            let url = ukv[0].links[0].href;
-            visit(url)
+        instanceSelect.onchange = function(event) {
+            instanceID = event.target.value
+            let url = `http://labs.metoffice.gov.uk/wotw/collections/${collectionID}/${instanceID}?outputFormat=application%2Fjson`
             getRequest(url, function(responseText) {
-                let instances = JSON.parse(responseText).instances
-                let latest = instances.filter((instance) => instance.id == 'latest');
-                let url = latest[0].links[0].href;
-                visit(url)
-                getRequest(url, function(responseText) {
-                    let obj = JSON.parse(responseText)
-                    let links = obj.collection.links.filter((l) => l.type == 'polygon')
-                    let url = links[0].href;
-                    visit(url)
-                    getRequest(url, function(responseText) {
-                        console.log(responseText)
-                    })
-                })
+                console.log(JSON.parse(responseText))
+                responseDiv.innerHTML = responseText
             })
-        })
+        }
     }
     return ns
 })();
